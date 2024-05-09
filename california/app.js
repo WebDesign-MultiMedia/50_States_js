6c0cb5a8eca49db0da67fe90da133c7ca2b2fb02
j.salas2016 <jsalas198016@gmail.com>
2024-05-08 22:25:20 -0400

    50 States js html css

---
+++ california/app.js
@@ -0,0 +1,5 @@
+const myName = "Julio";
+const visited = false;
+const moneySpent = 0;
+
+console.log(`It is a ${visited} statement that ${myName} visited california and spent ${moneySpent} dollars` )
---
+++ oregon/app.js
@@ -0,0 +1,5 @@
+const myName = "Julio";
+const visited = false;
+const moneySpent = 0;
+
+console.log(`It is a ${visited} statement that ${myName} visited oregon and spent ${moneySpent} dollars` )
